0f46e6682d7ea11b18e5e411b55eac2d647c8d65
ohdongmin <59468381+ohdongmin@users.noreply.github.com>
2024-04-03 23:19:05 +0900

    Update main_simple.py
    get_rsi로 통일 시킴

--- myworld/main_simple.py
+++ myworld/main_simple.py
@@ -48,7 +48,7 @@ def get_rsi(target_ticker, period=14):
     df['rsi'] = 100 - (100 / (1 + df['rs']))
     rsi = df['rsi']
 
-    return rsi
+    return rsi.iloc[-1]
 
 def send_line(msg):
     headers = {'Authorization':'Bearer '+token}
@@ -64,7 +64,7 @@ def get_transaction_amount(date, num):
         
         try :
             df = pyupbit.get_ohlcv(ticker, date)	# date 기간의 거래대금을 구해준다
-            volume_money =  df['close'][-1] * df['volume'][-1]  
+            volume_money =  df['close'].iloc[-1] * df['volume'].iloc[-1]  
             dic_ticker[ticker] = volume_money
         except Exception as e:
             pass
@@ -85,38 +85,21 @@ def get_transaction_amount(date, num):
 
 def searchRSI(settingRSI):
     try :
-        tickers = pyupbit.get_tickers(fiat="KRW")
+        #tickers = get_transaction_amount(1,20)
+        tickers = pyupbit.get_tickers(fiat="KRW")  #all finding
         for symbol in tickers : 
-            url = "https://api.upbit.com/v1/candles/minutes/10" #"https://api.upbit.com/v1/candles/days"
-            querystring = {"market":symbol,"count":"200"}
-            response = requests.request("GET", url, params=querystring)
-            data = response.json()
-            df = pd.DataFrame(data)
-            df=df.reindex(index=df.index[::-1]).reset_index()
-            df['close']=df["trade_price"]
-            def rsi(ohlc: pd.DataFrame, period: int = 14):
-                delta = ohlc["close"].diff()
-                up, down = delta.copy(), delta.copy()
-                up[up < 0] = 0
-                down[down > 0] = 0
-
-                _gain = up.ewm(alpha=1/period).mean()
-                _loss = down.abs().ewm(alpha=1/period).mean()
-
-                RS = _gain / _loss
-                return pd.Series(100 - (100 / (1 + RS)), name="RSI")
-            rsi = rsi(df, 14).iloc[-1]
+            rsi = get_rsi(symbol,14)
             if rsi < settingRSI :
                 send_line("\n!!과매도 현상 발견!!\n"+symbol)
-                print(f"매수 시점 rsi : {rsi}")
+                print(f"{symbol} 매수 시점 rsi : {rsi}")
+                time.sleep(1)
                 return symbol
-                break
-            time.sleep(1)
-        # print("settingRSI값을 5 높입니다.")
-        # searchRSI(settingRSI+5)
+                
+                
+            #time.sleep(1)
 
     except :
-        time.sleep(2)
+        time.sleep(1)
 
 
 # top5_tickers = get_transaction_amount("day", 5)	# 거래대금 상위 5개 코인 선정
@@ -140,42 +123,44 @@ while True:
     try : 
         target_RSI = 25
         sell_rsi = 60
-        while True:
+        TF = False
+        while TF == False:
             target_ticker = searchRSI(target_RSI)
             time.sleep(1)
-            if target_ticker :
+            if len(target_ticker) > 1:
                 buy(target_ticker,trade_money)
-                bought_rsi = get_rsi(target_ticker,14).iloc[-1]
+                bought_rsi = get_rsi(target_ticker,14)
                 TF = True
                 break
             else :
                 print(f"계속 탐색중입니다. Target RSI : {target_RSI}")
                 target_RSI += 1
-        firstprice =pyupbit.get_current_price(target_ticker)
         cnt = 1
         bp =False
         x = 0
         while TF == True :
             Coin_bought = upbit.get_balance(target_ticker)
             total_balance = Coin_bought * pyupbit.get_current_price(target_ticker) 
-            rsi_value = get_rsi(target_ticker,14).iloc[-1]
+            rsi_value = get_rsi(target_ticker,14)
             print(f"매도 rsi : {rsi_value} ")
             if total_balance > 100000:
                 send_line("매도 스킵합니다")
+                bp = True
                 break
             if Coin_bought<1:
                 send_line('already sold')
+                TF = False
                 break
             if total_balance < trade_money*cnt*0.95:
                 bp = True
                 sell(target_ticker)
                 break
             if rsi_value >60 :
-                time.sleep(300)
-                rsi_value = get_rsi(target_ticker,14).iloc[-1]
+                time.sleep(600)
+                rsi_value = get_rsi(target_ticker,14)
                 if rsi_value < sell_rsi -x:
                     sell(target_ticker)
-                    send_line(f'sell volume:{total_balance}')
+                    TF = False
                     break
                 else:
                     sell_rsi = max(rsi_value,sell_rsi)
